4d95d4359426a6079b7979a5f80132b6bd80d321
Luc Robbins <aidenrob1216@gmail.com>
2026-02-27 19:23:34 -0500

    Update updater-preload.js

--- src/updater-preload.js
+++ src/updater-preload.js
@@ -4,6 +4,7 @@ contextBridge.exposeInMainWorld('updaterAPI', {
   onStatus:   (cb) => ipcRenderer.on('update-status',   (e, d) => cb(d)),
   onProgress: (cb) => ipcRenderer.on('update-progress', (e, d) => cb(d)),
   onComplete: (cb) => ipcRenderer.on('update-complete', (e, d) => cb(d)),
+  downloadUpdate: () => ipcRenderer.send('download-update'),
   installNow: ()   => ipcRenderer.send('install-update'),
   skipUpdate: ()   => ipcRenderer.send('skip-update'),
 });
