22aeff8d2570c0c6cf9d4b31d7d3e30400f1b397
Luc Robbins <aidenrob1216@gmail.com>
2026-02-27 19:43:28 -0500

    Update updater-preload.js

--- src/updater-preload.js
+++ src/updater-preload.js
@@ -1,10 +1,14 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('updaterAPI', {
-  onStatus:   (cb) => ipcRenderer.on('update-status',   (e, d) => cb(d)),
-  onProgress: (cb) => ipcRenderer.on('update-progress', (e, d) => cb(d)),
-  onComplete: (cb) => ipcRenderer.on('update-complete', (e, d) => cb(d)),
-  downloadUpdate: () => ipcRenderer.send('download-update'),
-  installNow: ()   => ipcRenderer.send('install-update'),
-  skipUpdate: ()   => ipcRenderer.send('skip-update'),
+  // Listen for events from main process
+  onUpdateStatus:   (cb) => ipcRenderer.on('update-status',   (_, data) => cb(data)),
+  onUpdateProgress: (cb) => ipcRenderer.on('update-progress', (_, data) => cb(data)),
+  onUpdateComplete: (cb) => ipcRenderer.on('update-complete', () => cb()),
+  onUpdateError:    (cb) => ipcRenderer.on('update-error',    (_, data) => cb(data)),
+
+  // Send commands to main process
+  startDownload:    () => ipcRenderer.send('start-update-download'),
+  installUpdate:    () => ipcRenderer.send('install-update'),
+  skipUpdate:       () => ipcRenderer.send('skip-update'),
 });
